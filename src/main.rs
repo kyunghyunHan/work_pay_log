7ed6328ccb1eb84adc96f4910ea23ce2f97b33ce
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-06-04 21:55:48 +0900

    feat:Lstm init

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod model;
+
+fn main(){
+
+   
+}
---
+++ src/model/mod.rs
@@ -0,0 +1,12 @@
+use candle_nn::LSTM;
+
+pub struct LstmModel{
+   hidden_layer_size :i32,
+   lstm:LSTM,
+}
+
+impl LstmModel{
+    pub fn new(){
+
+    }
+}
