e3964d345ad23b2a235b0804a6fe17c34e212f6c
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-06-11 22:04:18 +0900

    feat:model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,45 +1,98 @@
-use candle_nn::{LSTM,RNN};
-use candle_core::{Result,Device,Tensor,test_utils::to_vec2_round,DType};
-pub struct LstmModel{
-   hidden_layer_size :i32,
-   lstm:LSTM,
+use candle_core::{Device, Tensor,DType};
+use candle_nn::{init, GRUConfig, Linear, VarBuilder, LSTM, RNN,LSTMConfig,VarMap,Optimizer};
+use std::collections::HashMap;
+#[derive(Debug)]
+struct Net {
+    layers: Vec<LSTM>,
+    // fc: Linear,
 }
+impl Net {
+   pub  fn new(vs: VarBuilder) -> candle_core::Result<Self> {
+        let config= LSTMConfig::default_no_bias();
+        let vb = &vs.pp("lstm");
+        let mut layers = vec![];
+        for layer_idx in 0..2 {
+            let config = candle_nn::LSTMConfig {
+                layer_idx,
+                ..Default::default()
+            };
+            let lstm = candle_nn::lstm(1, 5, config, vb.clone())?;
+            layers.push(lstm)
+        }
+        Ok(Self { layers })
+    }
 
-impl LstmModel{
-    pub fn new(){
-
+    fn forward(&self, xs: &Tensor) -> candle_core::Result<Tensor>{
+        let mut xs = xs.clone();
+        for layer in self.layers.iter() {
+            let states = layer.seq(&xs)?;
+            xs = layer.states_to_tensor(&states)?;
+        }
+        Ok(xs)
     }
 }
-fn main()->Result<()>{
-    let cpu = &Device::Cpu;
-    let w_ih = Tensor::arange(0f32, 24f32, cpu)?.reshape((12, 2))?;
-    let w_ih = w_ih.cos()?;
-    let w_hh = Tensor::arange(0f32, 36f32, cpu)?.reshape((12, 3))?;
-    let w_hh = w_hh.sin()?;
-    let b_ih = Tensor::new(
-        &[-1f32, 1., -0.5, 2., -1., 1., -0.5, 2., -1., 1., -0.5, 2.],
-        cpu,
-    )?;
-    let b_hh = b_ih.cos()?;
-    let tensors: std::collections::HashMap<_, _> = [
-        ("weight_ih_l0".to_string(), w_ih),
-        ("weight_hh_l0".to_string(), w_hh),
-        ("bias_ih_l0".to_string(), b_ih),
-        ("bias_hh_l0".to_string(), b_hh),
-    ]
-    .into_iter()
-    .collect();
-    let vb = candle_nn::VarBuilder::from_tensors(tensors, DType::F32, cpu);
-    let lstm = candle_nn::lstm(2, 3, Default::default(), vb)?;
-    let mut state = lstm.zero_state(1)?;
-    for inp in [3f32, 1., 4., 1., 5., 9., 2.] {
-        let inp = Tensor::new(&[[inp, inp * 0.5]], cpu)?;
-        state = lstm.step(&inp, &state)?
+pub async fn main() ->candle_core::Result<()> {
+    let input_str = "apple";
+    let label_str = "pple!";
+
+    let mut char_vocab: Vec<char> = input_str.chars().chain(label_str.chars()).collect();
+    char_vocab.sort();
+    char_vocab.dedup();
+    let vocab_size = char_vocab.len();
+    println!("문자 집합의 크기: {}", vocab_size);
+    let input_size = vocab_size; // 입력의 크기는 문자 집합의 크기
+    let hidden_size = 5;
+    let output_size = 5;
+    let learning_rate = 0.1;
+
+    let mut char_to_index: HashMap<char, usize> = HashMap::new();
+    for (i, &c) in char_vocab.iter().enumerate() {
+        char_to_index.insert(c, i);
     }
-    let h = state.h();
-    let c = state.c();
-    assert_eq!(to_vec2_round(h, 4)?, &[[0.9919, 0.1738, -0.1451]]);
-    assert_eq!(to_vec2_round(c, 4)?, &[[5.725, 0.4458, -0.2908]]);
+    println!("{:?}", char_to_index);
+
+    let mut index_to_char: HashMap<usize, char> = HashMap::new();
+    for (key, &value) in &char_to_index {
+        index_to_char.insert(value, *key);
+    }
+    println!("{:?}", index_to_char);
+
+    let x_data: Vec<usize> = input_str.chars().map(|c| char_to_index[&c]).collect();
+    let y_data: Vec<u32> = label_str
+        .chars()
+        .map(|c| char_to_index[&c] as u32)
+        .collect();
+
+    println!("{:?}", x_data);
+    println!("{:?}", y_data);
+
+    let mut x_one_hot = Vec::new();
+    for &x in &x_data {
+        x_one_hot.push(one_hot_encode(x, vocab_size));
+    }
+    println!("{:?}", x_one_hot);
+    let device = Device::Cpu;
+    let x = Tensor::new(x_one_hot, &device)?.unsqueeze(0)?;
+    let y = Tensor::new(y_data, &device)?.unsqueeze(0)?;
+    println!("{}", x);
+    println!("{}", y);
+    let varmap = VarMap::new();
+   
+    let vs = VarBuilder::from_varmap(&varmap, DType::F64, &device);
+
+    let net= Net::new(vs)?;
+  
+    println!("{:?}",net);
+    let mut sgd = candle_nn::SGD::new(varmap.all_vars(), learning_rate)?;
+    for i in 0..100{
+        let hypothesis = net.forward(&x)?;
+
+    }
     Ok(())
+}
 
+fn one_hot_encode(index: usize, vocab_size: usize) -> Vec<f64> {
+    let mut one_hot_row = vec![0.0; vocab_size];
+    one_hot_row[index] = 1.0;
+    one_hot_row
 }
